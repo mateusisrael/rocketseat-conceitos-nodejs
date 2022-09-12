44b82320cf34e3f36ee6b740d36b875c5436e6c1
mateusisrael <mateuss.i.fernandes@gmail.com>
2022-09-11 22:13:11 -0300

    Create users POST method

--- src/index.js
+++ src/index.js
@@ -1,21 +1,42 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const user = request.body.data
+  const userExists = users.find(item => user.username === item.username)
+
+  if(userExists !== undefined) {
+    response.status(400).json({ message: "Nome de usuário não disponível" })
+  }
+
+  next()
 }
 
-app.post('/users', (request, response) => {
-  // Complete aqui
+app.post('/users', checksExistsUserAccount, (request, response) => {
+  try {
+    const { data } = request.body
+
+    const user = {
+      id: uuidv4(),
+      name: data.name,
+      username: data.username,
+      todos: []
+    }
+  
+    users.push(user)
+    response.status(201).json(user)
+  } catch (error) {
+    response.status(500).send()
+  }
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
